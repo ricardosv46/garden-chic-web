5a793c33ce8469d6d44c319d479508a87b49d084
RICARDO SOLIS <ricardosv46@gmail.com>
2022-05-16 11:59:37 -0500

    feat: se agrega componente container y se avanza componente work

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import Banner from '../banner'
 import MenuDestokp from '../menu/desktop/index'
-import MenuMobile from '../menu/mobile/index'
+import MenuMobile from '../menu/mobile'
 
 const Header = () => {
-	return (
-		<div className=''>
-			<div className='hidden lg:block'>
-				<MenuDestokp />
-			</div>
-			<div className='lg:hidden relative z-20'>
-				<MenuMobile />
-			</div>
-		</div>
-	)
+  return (
+    <div className=''>
+      <div className='hidden lg:block'>
+        <MenuDestokp />
+      </div>
+      <div className='lg:hidden relative z-20'>
+        <MenuMobile />
+      </div>
+    </div>
+  )
 }
 
 export default Header
