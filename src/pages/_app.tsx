9bb7109cdcd00600b8c9211213baee85ab1876e1
RICARDO SOLIS <ricardosv46@gmail.com>
2022-05-16 17:19:40 -0500

    fix: solucionando problema de react 18

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/header'
+import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [showChild, setShowChild] = useState(false)
+  useEffect(() => {
+    setShowChild(true)
+  }, [])
+  if (!showChild) {
+    return null
+  }
   return (
     <>
-      <Header />
       <Component {...pageProps} />
     </>
   )
