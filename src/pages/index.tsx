d695c715839d349986966f997044ef3a09f5bcc3
RICARDO SOLIS <ricardosv46@gmail.com>
2022-05-28 11:12:03 -0500

    feat: agregando metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,56 +11,24 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        {/* <title>Garden Chic - Inicio</title>
-        <meta
-          name='description'
-          content='Somos expertos en el cuidado de tu jardin.'
-        />
-        <meta name='keywords' content='jardines, jardin, plantas' /> */}
-
-        {/* <meta property='og:title' content='Garden Chic - meta' />
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://garden-chic-web.vercel.app' />
-        <meta
-          property='og:image'
-          content='https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg'
-        />
-        
-
-        <meta name='twitter:card' content='summary' />
-        <meta name='twitter:site' content='website' />
-        <meta
-          name='twitter:title'
-          content='Small Island Developing States Photo Submission'
-        />
-        <meta name='twitter:description' content='View the album on Flickr.' />
-        <meta
-          name='twitter:image'
-          content='https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg'
-        /> */}
-
-        {/* <!-- HTML Meta Tags --> */}
-
+        {/* configurar metatags para facebook , whatsapp e instagram  */}
         <title>Garden Chic - Inicio</title>
         <meta
           name='description'
           content='Somos expertos en el cuidado de tu jardin.'
         />
-
-        {/* <!-- Facebook Meta Tags --> */}
-        <meta property='og:url' content='https://garden-chic-web.vercel.app/' />
-        <meta property='og:type' content='website' />
+        <meta name='keywords' content='jardines, jardin, plantas' />
         <meta property='og:title' content='Garden Chic - Inicio' />
-        <meta
-          property='og:description'
-          content='Somos expertos en el cuidado de tu jardin.'
-        />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://garden-chic-web.vercel.app/' />
         <meta
           property='og:image'
           content='https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg'
         />
-
-        {/* <!-- Twitter Meta Tags --> */}
+        <meta
+          property='og:description'
+          content='Somos expertos en el cuidado de tu jardin.'
+        />
         <meta name='twitter:card' content='summary_large_image' />
         <meta property='twitter:domain' content='garden-chic-web.vercel.app' />
         <meta
@@ -73,11 +41,9 @@ const Home: NextPage = () => {
           content='Somos expertos en el cuidado de tu jardin.'
         />
         <meta
-          name='twitter:image'
+          property='twitter:image'
           content='https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg'
         />
-
-        {/* <!-- Meta Tags Generated via https://www.opengraph.xyz --> */}
       </Head>
 
       <Banner />
