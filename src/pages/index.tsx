01343ff1929abbec2180b3ab7d00532f4b9cfe7e
RICARDO SOLIS <ricardosv46@gmail.com>
2022-05-28 11:47:09 -0500

    feat: se agrega op

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,40 +11,42 @@ import { NextSeo } from 'next-seo'
 const Home: NextPage = () => {
   return (
     <>
-      {/* <Head>
+      <Head>
         <title>Garden Chic - Inicio</title>
         <meta
           name='description'
           content='Somos expertos en el cuidado de tu jardin.'
         />
-        <meta name='keywords' content='jardines, jardin, plantas' />
-      </Head> */}
 
-      <NextSeo
-        title='Garden Chic - Inicio'
-        description='Somos expertos en el cuidado de tu jardin.'
-        canonical='https://garden-chic-web.vercel.app/'
-        openGraph={{
-          type: 'website',
-          url: 'https://garden-chic-web.vercel.app',
-          title: 'Garden Chic',
-          description: 'Somos expertos en el cuidado de tu jardin.',
-          site_name: 'Garden Chic',
-          images: [
-            {
-              url: 'https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg',
-              width: 800,
-              height: 420,
-              alt: 'Garden Chic'
-            }
-          ]
-        }}
-        // twitter={{
-        //   handle: '@avneesh0612',
-        //   site: '@avneesh0612',
-        //   cardType: 'summary_large_image'
-        // }}
-      />
+        <meta property='og:url' content='https://garden-chic-web.vercel.app/' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Garden Chic - Inicio' />
+        <meta
+          property='og:description'
+          content='Somos expertos en el cuidado de tu jardin.'
+        />
+        <meta
+          property='og:image'
+          content='https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg'
+        />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:domain' content='garden-chic-web.vercel.app' />
+        <meta
+          property='twitter:url'
+          content='https://garden-chic-web.vercel.app/'
+        />
+        <meta name='twitter:title' content='Garden Chic - Inicio' />
+        <meta
+          name='twitter:description'
+          content='Somos expertos en el cuidado de tu jardin.'
+        />
+        <meta
+          name='twitter:image'
+          content='https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg'
+        />
+      </Head>
+
       <Banner />
       <QuePodemosHacer />
       <Servicios />
