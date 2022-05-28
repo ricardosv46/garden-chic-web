6d1e26a1f5eaf07b2ff58373d9813894666dfa78
RICARDO SOLIS <ricardosv46@gmail.com>
2022-05-28 10:54:22 -0500

    feat: agregando metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,18 +18,14 @@ const Home: NextPage = () => {
         />
         <meta name='keywords' content='jardines, jardin, plantas' /> */}
 
-        <meta property='og:title' content='Garden Chic - meta' />
+        {/* <meta property='og:title' content='Garden Chic - meta' />
         <meta property='og:type' content='website' />
         <meta property='og:url' content='https://garden-chic-web.vercel.app' />
         <meta
           property='og:image'
           content='https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg'
         />
-        {/* <meta name='twitter:card' content='summary_large_image' />
-        <meta
-          name='twitter:image'
-          content='https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg'
-        /> */}
+        
 
         <meta name='twitter:card' content='summary' />
         <meta name='twitter:site' content='website' />
@@ -41,7 +37,54 @@ const Home: NextPage = () => {
         <meta
           name='twitter:image'
           content='https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg'
+        /> */}
+
+        {/* <!-- HTML Meta Tags --> */}
+        <title>
+          Buy best disposal products from Garima disposal and plastic in Jaipur
+        </title>
+        <meta
+          name='description'
+          content='Buy best disposal products at low prices. Paper, plastic and wooden products. Buy best thermocol products, bowls, cups, glasses, pattal, dona, food containers etc.'
         />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property='og:url' content='https://garden-chic-web.vercel.app/' />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:title'
+          content='Buy best disposal products from Garima disposal and plastic in Jaipur'
+        />
+        <meta
+          property='og:description'
+          content='Buy best disposal products at low prices. Paper, plastic and wooden products. Buy best thermocol products, bowls, cups, glasses, pattal, dona, food containers etc.'
+        />
+        <meta
+          property='og:image'
+          content='https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg'
+        />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name='twitter:card' content='summary_large_image' />
+        {/* <meta property='twitter:domain' content='garimadisposal.in' /> */}
+        <meta
+          property='twitter:url'
+          content='https://garden-chic-web.vercel.app/'
+        />
+        <meta
+          name='twitter:title'
+          content='Buy best disposal products from Garima disposal and plastic in Jaipur'
+        />
+        <meta
+          name='twitter:description'
+          content='Buy best disposal products at low prices. Paper, plastic and wooden products. Buy best thermocol products, bowls, cups, glasses, pattal, dona, food containers etc.'
+        />
+        <meta
+          name='twitter:image'
+          content='https://garden-chic-web.vercel.app/imgs/banner/banner__item1.jpg'
+        />
+
+        {/* <!-- Meta Tags Generated via https://www.opengraph.xyz --> */}
       </Head>
 
       <Banner />
