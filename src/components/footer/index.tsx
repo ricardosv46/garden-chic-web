268be4d0e45ceac61d635c221cccd4dd07f8afa8
jeanflores99 <ajiliof@gmail.com>
2022-08-15 23:14:36 -0500

    se añadio la imagen al footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,12 +1,15 @@
-import Container from '@components/container'
 import React from 'react'
+import { FaFacebook, FaInstagram, FaPinterest, FaLinkedin } from 'react-icons/fa'
+import Container from '@components/container'
+import Image from 'next/image'
+
 
 export const Footer = () => {
     return (
         <Container bgColor='bg-[#004969]'>
-            <div className='flex flex-col md:flex-row  text-white py-8'>
-                <div className='text-[14px] md:w-1/4'>
-                    <div className=' tracking-[1px] capitalize my-4 leading-[23.24px]'>Nosotros</div>
+            <div className='flex flex-col md:flex-row  text-white py-8 text-[16px] gap-6'>
+                <div className=' md:w-1/4'>
+                    <div className=' tracking-[1px] capitalize my-3 leading-[23.24px]'>Nosotros</div>
                     <ul className='flex flex-col gap-1 leading-[18.2px] '>
                         <li>Conócenos</li>
                         <li>Misión</li>
@@ -16,8 +19,8 @@ export const Footer = () => {
                         <li>Blog</li>
                     </ul>
                 </div>
-                <div className='text-[14px] md:w-1/4'>
-                    <div className=' tracking-[1px] capitalize my-4 leading-[23.24px]'>Servicios</div>
+                <div className='] md:w-1/4'>
+                    <div className=' tracking-[1px] capitalize my-3 leading-[23.24px]'>Servicios</div>
                     <ul className='flex flex-col gap-1 leading-[18.2px] '>
                         <li>Empresas de jardinería en Lima</li>
                         <li>Jardín urbano Perú</li>
@@ -28,8 +31,8 @@ export const Footer = () => {
                         <li>Viveros en Lurín</li>
                     </ul>
                 </div>
-                <div className='text-[14px] md:w-1/4'>
-                    <div className=' tracking-[1px] capitalize my-4 leading-[23.24px]'>Servicios</div>
+                <div className=' md:w-1/4'>
+                    <div className=' tracking-[1px] capitalize my-3  leading-[23.24px]'>Servicios</div>
                     <ul className='flex flex-col gap-1 leading-[18.2px] '>
                         <li>Empresas de jardinería en Lima</li>
                         <li>Jardín urbano Perú</li>
@@ -40,16 +43,27 @@ export const Footer = () => {
                         <li>Viveros en Lurín</li>
                     </ul>
                 </div>
-                <div className='text-[14px] md:w-1/4'>
-                    <div className=' tracking-[1px] capitalize my-4 leading-[23.24px]'>Síguenos</div>
-                    <ul className='flex flex-col gap-1 leading-[18.2px] '>
-                        <li>Conócenos</li>
-                        <li>Misión</li>
-                        <li>Visión</li>
-                        <li>Valores</li>
-                        <li>Objetivos</li>
-                        <li>Blog</li>
+                <div className='md:w-1/4 flex flex-col gap-4'>
+                    <div className=' tracking-[1px] capitalize my-3 leading-[23.24px]'>Síguenos</div>
+                    <ul className='flex flex-wrap  leading-[18.2px] gap-4 text-primary-300'>
+                        <li><FaFacebook className='w-6 h-6 hover:text-white' /></li>
+                        <li><FaLinkedin className='w-6 h-6 hover:text-white' /></li>
+                        <li><FaInstagram className='w-6 h-6 hover:text-white' /></li>
+                        <li><FaPinterest className='w-6 h-6 hover:text-white' /></li>
                     </ul>
+                    <div className='flex flex-col'>
+                        <span>Tel: 996 276 109</span>
+                        <span className='text-[14px]'>consultas@gardenchicperu.com</span>
+                    </div>
+                    <div className='w-full h-16 relative'>
+                        <Image
+                            className='absolute w-full h-full'
+                            loading='lazy'
+                            layout='fill'
+                            objectFit='contain'
+                            src={'/imgs/footer/LOGO_HORIZONTAL.jpg'}
+                        />
+                    </div>
                 </div>
             </div>
         </Container>
