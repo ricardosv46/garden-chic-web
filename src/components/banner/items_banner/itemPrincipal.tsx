b88d77a51cf6fd5a9bb571c9ad5a6acb89685360
jeanflores99 <ajiliof@gmail.com>
2022-08-27 11:35:24 -0500

    arreglando animaciones banner

--- src/components/banner/items_banner/itemPrincipal.tsx
+++ src/components/banner/items_banner/itemPrincipal.tsx
@@ -14,13 +14,13 @@ export const ItemPrincipal = ({ data }: Iprops) => {
             <div className='mx-auto my-0 w-[90%] xl:w-[1280px]  h-full flex justify-center items-center text-white relative z-10'>
                 <div className='h-80 flex flex-col items-center justify-center lg:max-w-[700px] '>
                     {/*  animate-renderTranslate  */}
-                    <span className='font-bold text-xl md:font-extralight translateRender'>
+                    <span className='font-bold text-lg sm:text-xl md:font-extralight translateRender'>
                         {data.textheader || ''}
                     </span>
-                    <h4 className='font-bold text-3xl md:text-7xl md:font-extralight '>
+                    <h4 className='font-bold text-2xl sm:text-3xl md:text-7xl md:font-extralight '>
                         {data.tittle || ''}
                     </h4>
-                    <p className='mb-10 mt-5 text-sm md:text-2xl  w-3/4 text-center lg:w-1/2'>
+                    <p className='mb-10 mt-5 text-xs sm:text-sm md:text-2xl  w-3/4 text-center lg:w-1/2'>
                         {data.subtittle || ''}
                     </p>
                 </div>
