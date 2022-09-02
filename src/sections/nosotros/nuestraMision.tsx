0301fe2871c09534a272dcce50afe3cdaa2a4703
jeanflores99 <ajiliof@gmail.com>
2022-09-02 12:58:13 -0500

    levantando observaciones del

--- src/sections/nosotros/nuestraMision.tsx
+++ src/sections/nosotros/nuestraMision.tsx
@@ -4,7 +4,7 @@ import style from './nuestraMision.module.css'
 
 const NuestraMision = () => {
   return (
-    <Container bgColor='bg-gray-100' className='py-5 pb-10 '>
+    <Container bgColor='bg-gray-100' className='py-5 pb-10'>
       {/* <div className='grid grid-cols-1 md:grid-cols-[60%,40%] bg-white'>
           <div className='order-2   py-3 px-6 text-xl md:text-2xl  md:px-12 lg:py-10 xl:pt-32 xl:pb-24  font-light text-primary-800'>
             <h2 className='font-bold leading-none text-xl md:text-2xl lg:text-4xl'>
@@ -22,35 +22,23 @@ const NuestraMision = () => {
             <div className={`${style['img-nuestraMision']} w-full `}></div>
           </div>
         </div> */}
-      <div className='bg-white'>
-        <div className='pt-10 pl-10 pr-10 pb-0 hidden md:block '>
-          <div className='grid grid-cols-2'>
-            <div></div>
-            <h2 className='font-bold leading-none text-xl md:text-2xl lg:text-4xl text-center '>
-              Nuestra misión
-            </h2>
-          </div>
+
+      <div className='grid grid-cols-1 md:grid-cols-[50%,50%] bg-white '>
+        <div className='order-2  py-3 px-6 text-xl lg:text-2xl h-full p-10 md:px-12 lg:py-10  font-light text-primary-800 '>
+          <h2 className='font-bold leading-none text-xl md:text-2xl lg:text-4xl'>
+            Nuestra misión
+          </h2>
+          <p className='text-xs md:text-lg lg:text-2xl font-extralight mt-8 text-justify ' >
+            Servir a nuestros clientes de una manera excelente, tratando
+            de reflejar lo más fielmente posible el estilo, la decoración,
+            los colores y mantenimiento del área verde, donde se vea reflejado las
+            expectativas logradas en obra y diseño a través del jardín. Con el objetivo de superar sus expectativas
+
+          </p>
         </div>
-        <div className='grid grid-cols-1 md:grid-cols-[50%,50%] p-6 lg:p-10 gap-4 md:gap-7'>
+        <div className='order-1   p-10 '>
+          <div className={`w-full bg-top bg-no-repeat bg-cover h-[250px] md:h-[450px] `} style={{ backgroundImage: `url('/imgs/nosotros/header/03_NUESTRA_MISION.jpg')` }} />
 
-          <div className='order-2  text-xl font-light text-primary-800 md:p-2 '>
-            <h2 className='font-bold leading-none text-xl md:text-2xl lg:text-4xl mb-4 md:mb-6 md:hidden'>
-              Nuestra misión
-            </h2>
-            <div className='h-full md:pr-12'>
-              <p className='text-xs md:text-xl lg:text-2xl font-extralight text-justify ' >
-                Servir a nuestros clientes de una manera excelente, tratando
-                de reflejar lo más fielmente posible el estilo, la decoración,
-                los colores y mantenimiento del área verde, donde se vea reflejado las
-                expectativas logradas en obra y diseño a través del jardín. Con el objetivo de superar sus expectativas
-              </p>
-
-            </div>
-          </div>
-          <div className='order-1 flex justify-end items-end pr-4 ' >
-            {/* <div className={`${style['img-porqueElegirnos']} w-full border-2 border-black`} /> */}
-            <div className={`w-full bg-top bg-no-repeat bg-cover h-[250px] md:h-[400px] `} style={{ backgroundImage: `url('/imgs/nosotros/header/03_NUESTRA_MISION.jpg')` }} />
-          </div>
         </div>
       </div>
     </Container>
--- src/sections/nosotros/porqueElegirnos.tsx
+++ src/sections/nosotros/porqueElegirnos.tsx
@@ -5,10 +5,10 @@ import style from './porqueElegirnos.module.css'
 const PorqueElegirnos = () => {
   return (
     <Container bgColor='bg-gray-100' className='pt-10 pb-5'>
-      {/* <div className='flex flex-col bg-white'>
-  
-      {/* <div className='grid grid-cols-1 md:grid-cols-[50%,50%] bg-white '>
-        <div className='order-2 md:order-1 py-3 px-6 text-xl lg:text-2xl p-10 md:px-12 lg:py-10 xl:pt-28 xl:pb-20 font-light text-primary-800 '>
+      {/* <div className='flex flex-col bg-white'> */}
+
+      <div className='grid grid-cols-1 md:grid-cols-[50%,50%] bg-white '>
+        <div className='order-2 md:order-1 py-3 px-6 text-xl lg:text-2xl h-full p-10 md:px-12 lg:py-10  font-light text-primary-800 '>
           <h2 className='font-bold leading-none text-xl md:text-2xl lg:text-4xl'>
             ¿Por qué elegirnos?
           </h2>
@@ -19,15 +19,16 @@ const PorqueElegirnos = () => {
             <span className='font-bold'> CASACOR Perú</span> desde el 2018, también formamos parte de la
             <span className='font-bold'> Asociación de Diseñadores del Perú</span> y de
             <span className='font-bold'> Perú Green Building Council</span> que tiene como objetivo principal el promover la
-            reducción de las emisiones de carbono implementando Edificios Sostenibles.  
+            reducción de las emisiones de carbono implementando Edificios Sostenibles.
 
           </p>
         </div>
-        <div className='order-1 md:order-2 flex justify-end items-end p-10 '>
-          <div className={`${style['img-porqueElegirnos']} w-full border-4`} ></div>
+        <div className='order-1 md:order-2  p-10 '>
+          <div className={`w-full bg-top bg-no-repeat bg-cover h-[250px] md:h-full `} style={{ backgroundImage: `url('/imgs/nosotros/header/02_POR_ELEGIRNOS.jpg')` }} />
+
         </div>
-      </div> */}
-      <div className='bg-white'>
+      </div>
+      {/* <div className='bg-white'>
         <div className='pt-10 pl-10 pr-10 pb-0 hidden md:block'>
           <h2 className='font-bold leading-none text-xl md:text-2xl lg:text-4xl  '>
             ¿Por qué elegirnos?
@@ -53,11 +54,10 @@ const PorqueElegirnos = () => {
             </div>
           </div>
           <div className='order-1 md:order-2 flex justify-end items-end pr-4 md:pr-12' >
-            {/* <div className={`${style['img-porqueElegirnos']} w-full border-2 border-black`} /> */}
             <div className={`w-full bg-top bg-no-repeat bg-cover h-[250px] md:h-full `} style={{ backgroundImage: `url('/imgs/nosotros/header/02_POR_ELEGIRNOS.jpg')` }} />
           </div>
         </div>
-      </div>
+      </div> */}
 
     </Container >
   )
