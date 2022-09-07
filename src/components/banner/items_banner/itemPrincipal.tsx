a6982c56ae60225d5562742a88ba016bcd367a24
jeanflores99 <ajiliof@gmail.com>
2022-09-07 17:18:22 -0500

    levantando observaciones del texto encontrado por jimy

--- src/components/banner/items_banner/itemPrincipal.tsx
+++ src/components/banner/items_banner/itemPrincipal.tsx
@@ -11,18 +11,18 @@ export const ItemPrincipal = ({ data }: Iprops) => {
     const ResolverPahtname = (imgCurrent: string) => `url(${pathnameImg}${imgCurrent})`
 
     return (
-        <div className={` ${style.itemBanner}`} style={{ backgroundImage: ResolverPahtname(data.image) }}  >
-            <div className='mx-auto my-0 w-[90%] xl:w-[1280px]  h-full flex justify-center items-center text-white relative z-10'>
+        <div className={` ${style.itemBanner} text-white`} style={{ backgroundImage: ResolverPahtname(data.image) }}  >
+            <div className='mx-auto my-0 w-[90%] xl:w-[1280px]  h-full flex justify-center items-center  relative z-10'>
                 <div className='h-80  flex flex-col  sm:gap-1 md:gap-2 lg:gap-4 items-center justify-center lg:max-w-[700px] absolute'>
                     {/*  animate-renderTranslate  */}
-                    <span className='font-bold text-[10px] sm:text-xl md:font-extralight translateRender font-garden_bold'>
+                    <span className='font-bold text-[10px] sm:text-xl lg:text-4xl md:font-extralight translateRender font-garden_regular '>
                         {data.textheader || ''}
                     </span>
                     {/*
                     <h4 className='font-bold text-base sm:text-3xl md:text-7xl md:font-extralight font-garden_bold '>
                         {data.tittle || ''}
                     </h4> */}
-                    <div className='relative w-[60%] h-14 sm:w-[50%] sm:h-16 md:h-24 lg:h-40 lg:w-[85%]  '>
+                    <div className='relative w-[60%] h-14 sm:w-[50%] sm:h-16 md:h-24 lg:h-40 lg:w-[80%]  '>
                         <Image
                             className='absolute w-full h-full '
                             loading='lazy'
@@ -33,7 +33,7 @@ export const ItemPrincipal = ({ data }: Iprops) => {
                     </div>
                     {/* <Image src={`${pathnameImg}${data.imgTittle!}`} width={300} height={2   00} alt='Logo_Banner' /> */}
 
-                    <p className='mb-10  text-[10px] sm:text-sm md:text-2xl  w-3/4 text-center lg:w-2/3 font-garden_medium text-gray-100 block'>
+                    <p className='m  text-[10px] sm:text-sm md:text-2xl  w-3/4 text-center lg:w-[53%] font-garden_medium  block'>
                         {data.subtittle || ''}
                     </p>
                 </div>
--- src/data/dataServicios.ts
+++ src/data/dataServicios.ts
@@ -28,8 +28,8 @@ export const servicios: EntityServicios[] = [
       {
         tittle: '¿Cómo podemos ayudarte?',
         subcontent: [
-          'Podemos ayudarte en la creación o mejora de tu espacio verde. Hemos diseñado, modificado e implementado espacios con características visibles, físicas y anímicas, tanto rural como urbano, haciendo relucir la jardinería de flora y fauna.',
-          'Nuestro equipo a realizado distintos proyectos paisajistas, logrando cultivar plantas como un arte, con el propósito de embellecer el entorno del proyecto y esto lo hacemos en compañía de nuestros clientes acercándonos, escuchándolos hasta lograr satisfacerlo más allá de sus expectativas.',
+          'Podemos ayudarte en la creación o mejora de tu espacio verde. Hemos diseñado, modificado e implementado espacios con características visibles, físicas y anímicas, tanto rural como urbano. Haciendo relucir la jardinería de flora y fauna.',
+          'Nuestro equipo a realizado distintos proyectos paisajistas, logrando cultivar plantas como un arte, con el propósito de embellecer el entorno del proyecto. Y esto lo hacemos en compañía de nuestros clientes acercándonos, escuchándolos hasta lograr satisfacerlo más allá de sus expectativas.',
           'Trabajamos nuestros proyectos como únicos, como lo son: “Un bello entorno paisajista”. Para ello contamos con un vivero y con técnicos, consultores, ingenieros y proveedores aliados que tienen el mismo objetivo en común: “hacer de tu entorno verde una obra de arte natural”',
         ],
       },
@@ -54,8 +54,8 @@ export const servicios: EntityServicios[] = [
       {
         tittle: '¿Cómo podemos ayudarte?',
         subcontent: [
-          'Hemos realizado más de 800 metros cuadrados de jardines verticales utilizando de manera eficiente más espacios del local o residencia, ya sea en el interior o exterior de cualquier pared, los beneficios de estos muros verdes como suelen llamarse son muy significativos como bajar algunos grados de temperatura más rápidamente, reducen el efecto de “isla de calor” y absorben partículas contaminantes del aire, haciendo un ambiente mejor, más sano, más productivo y sociable.',
-          'Nuestro equipo especializado en jardines verticales a realizar distintos proyectos que cada uno tiene su reto particular, por la altura, posición o espacio, logrando el objetivo de realizar una obra sin igual de arte natural.',
+          'Hemos realizado más de 800 metros cuadrados de jardines verticales utilizando de manera eficiente más espacios del local o residencia. Ya sea en el interior o exterior de cualquier pared. Los beneficios de estos muros verdes como suelen llamarse son muy significativos como bajar algunos grados de temperatura más rápidamente, reducen el efecto de “isla de calor” y absorben partículas contaminantes del aire. Haciendo un ambiente mejor, más sano, más productivo y sociable.',
+          'Nuestro equipo especializado en jardines verticales a realizar distintos proyectos que cada uno tiene su reto particular, por la altura, la posición o espacio. Logrando el objetivo de realizar una obra sin igual de arte natural.',
           'Nuestros proyectos en jardines verticales cuenta con técnicos, profesionales y proveedores que hacen la diferencia en la ejecución de la obra.',
         ],
       },
@@ -74,7 +74,7 @@ export const servicios: EntityServicios[] = [
       {
         tittle: '¿Cómo podemos ayudarte?',
         subcontent: [
-          'Nuestro servicio cuenta con un diagnóstico inicial del área verde, en ella consideramos el tipo de planta, la frecuencia y abundancia del riego para estas plantas, las plantas están enfermas o plagadas, existen plantas muertas, hay mala hierba o plantas invasoras, el estado del suelo, con ello se realiza la lista de tareas por realizar de acuerdo a la prioridad y mejora del espacio verde.',
+          'Nuestro servicio cuenta con un diagnóstico inicial del área verde, en ella consideramos el tipo de planta, la frecuencia y abundancia del riego para estas plantas, las plantas están enfermas o plagadas, existen plantas muertas, hay mala hierba o plantas invasoras, el estado del suelo. Con ello se realiza la lista de tareas por realizar de acuerdo a la prioridad y mejora del espacio verde.',
           'Nuestra propuesta de valor diferencial en este servicio es que destinamos un mismo personal capacitado como la asesoría en caso se requiera y una atención personalizada para atender los casos particulares que puedan tener como solucionarlas con prontitud.',
           'Trabajamos con un equipo especializado en el mantenimiento de las áreas verdes, con este servicio iniciamos nuestras labores en la empresa y es el servicio que se destaca por mantener la Salud y Estética de tus áreas verdes.',
         ],
@@ -94,15 +94,15 @@ export const servicios: EntityServicios[] = [
       {
         tittle: '¿Cómo podemos ayudarte?',
         subcontent: [
-          'Cada instalación de Techo Verde cuenta con un objetivo particular en la cual puede ser un aislador en la parte alta de una casa o edificio, para ello también se verifica su viabilidad, compuesto por una cubierta vegetal estratificada, cada capa contribuye al aislamiento, para que no drene agua y cree una superficie para el crecimiento de las plantas.',
-          'Contamos con una basta experiencia en este tipo de instalaciones, consideramos el acceso para la ejecución, el mantenimiento que traerá consigo y las condiciones del sitio como es la exposición al viento, el riesgo por algún inconveniente y la resistencia de las plantas.',
-          'Trabajamos nuestros proyectos como únicos, para ello contamos con un vivero y con técnicos, consultores, ingenieros y proveedores aliados que tienen el mismo objetivo en común: “hacer de tu entorno verde una obra de arte natural”.',
+          'Cada instalación de Techo Verde cuenta con un objetivo particular en la cual puede ser un aislador en la parte alta de una casa o edificio, para ello también se verifica su viabilidad. Compuesto por una cubierta vegetal estratificada, cada capa contribuye al aislamiento, para que no drene agua y cree una superficie para el crecimiento de las plantas.',
+          'Contamos con una basta experiencia en este tipo de instalaciones. Consideramos el acceso para la ejecución, el mantenimiento que traerá consigo y las condiciones del sitio como es la exposición al viento, el riesgo por algún inconveniente y la resistencia de las plantas.',
+          'Trabajamos nuestros proyectos como únicos. Para ello contamos con un vivero y con técnicos, consultores, ingenieros y proveedores aliados que tienen el mismo objetivo en común: “hacer de tu entorno verde una obra de arte natural”.',
         ],
       },
     ],
   },
   {
-    title: 'Sistema de Riego Tecnificado',
+    title: 'Riego Tecnificado',
     tittleResumido: 'Riego Tecnificado',
     position: 'l',
     img: 'riegotecnificado/01_PRINCIPAL.jpg',
@@ -116,7 +116,7 @@ export const servicios: EntityServicios[] = [
         subcontent: [
           'Nuestro sistema de riego tecnificado tiene el objetivo de generar más eficiencia del consumo de agua y reducir el impacto sobre el área verde. ',
           'La disposición y distribución de forma homogénea del agua genera la eficiencia de este recurso tan valioso para el mejor rendimiento de las plantas, haciendo un ambiente propicio de Salud y Estética de la jardinería de flora y fauna.',
-          'Trabajamos nuestros proyectos como únicos, para ello contamos con un vivero y con técnicos, consultores, ingenieros y proveedores aliados que tienen el mismo objetivo en común: “hacer de tu entorno verde una obra de arte natural”.',
+          'Trabajamos nuestros proyectos como únicos. Para ello contamos con un vivero y con técnicos, consultores, ingenieros y proveedores aliados que tienen el mismo objetivo en común: “hacer de tu entorno verde una obra de arte natural”.',
         ],
       },
     ],
@@ -134,9 +134,9 @@ export const servicios: EntityServicios[] = [
       {
         tittle: '¿Cómo podemos ayudarte?',
         subcontent: [
-          'Podemos ayudarte en la creación o mejora de tu espacio verde; Hemos diseñado, modificado e implementado espacios con características visibles, físicas y anímicas, tanto rural como urbano, haciendo relucir la jardinería de flora y fauna.',
-          'Nuestro equipo ha realizado distintos proyectos paisajistas, logrando cultivar plantas como un arte, con el propósito de embellecer el entorno del proyecto y esto lo hacemos en compañía de nuestros clientes acercándonos, escuchándolos hasta lograr satisfacerlo más allá de sus expectativas.',
-          'Trabajamos nuestros proyectos como únicos, como lo son: “Un bello entorno paisajista”, para ello contamos con un vivero y con técnicos, consultores, ingenieros y proveedores aliados que tienen el mismo objetivo en común: “hacer de tu entorno verde una obra de arte natural”',
+          'Podemos ayudarte en la creación o mejora de tu espacio verde. Hemos diseñado, modificado e implementado espacios con características visibles, físicas y anímicas, tanto rural como urbano. Haciendo relucir la jardinería de flora y fauna.',
+          'Nuestro equipo ha realizado distintos proyectos paisajistas. Logrando cultivar plantas como un arte, con el propósito de embellecer el entorno del proyecto. Esto lo hacemos en compañía de nuestros clientes acercándonos, escuchándolos hasta lograr satisfacerlo más allá de sus expectativas.',
+          'Trabajamos nuestros proyectos como únicos, como lo son: “Un bello entorno paisajista”. Para ello contamos con un vivero y con técnicos, consultores, ingenieros y proveedores aliados que tienen el mismo objetivo en común: “hacer de tu entorno verde una obra de arte natural”',
         ],
       },
     ],
@@ -154,9 +154,9 @@ export const servicios: EntityServicios[] = [
       {
         tittle: '¿Cómo podemos ayudarte?',
         subcontent: [
-          'Podemos ayudarte en la creación o mejora de tu espacio verde, hemos diseñado, modificado e implementado espacios con características visibles, físicas y anímicas, tanto rural como urbano, haciendo relucir la jardinería de flora y fauna.',
-          'Nuestro equipo ha realizado distintos proyectos paisajistas. Logrando cultivar plantas como un arte, con el propósito de embellecer el entorno del proyecto y esto lo hacemos en compañía de nuestros clientes acercándonos, escuchándolos hasta lograr satisfacerlo más allá de sus expectativas.',
-          'Trabajamos nuestros proyectos como únicos, como lo son: “Un bello entorno paisajista”, para ello contamos con un vivero y con técnicos, consultores, ingenieros y proveedores aliados que tienen el mismo objetivo en común: “hacer de tu entorno verde una obra de arte natural”.',
+          'Podemos ayudarte en la creación o mejora de tu espacio verde. Hemos diseñado, modificado e implementado espacios con características visibles, físicas y anímicas, tanto rural como urbano. Haciendo relucir la jardinería de flora y fauna.',
+          'Nuestro equipo ha realizado distintos proyectos paisajistas. Logrando cultivar plantas como un arte, con el propósito de embellecer el entorno del proyecto. Y esto lo hacemos en compañía de nuestros clientes acercándonos, escuchándolos hasta lograr satisfacerlo más allá de sus expectativas.',
+          'Trabajamos nuestros proyectos como únicos, como lo son: “Un bello entorno paisajista”. Para ello contamos con un vivero y con técnicos, consultores, ingenieros y proveedores aliados que tienen el mismo objetivo en común: “hacer de tu entorno verde una obra de arte natural”.',
         ],
       },
     ],
