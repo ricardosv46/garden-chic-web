64665813ab64ab8d3b4d5c5c149e1ce4fed3c8a9
ricardosv46 <ricardosv46@gmail.com>
2022-09-16 17:45:50 -0500

    feat: se agrega autocomplete

--- src/components/forms/FormEnvio.tsx
+++ src/components/forms/FormEnvio.tsx
@@ -1,93 +1,76 @@
-import { SelectSearch } from "@components/SelectSearch";
-import { useRouter } from "next/router";
-import React, { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
-import { useEffect } from "react";
-import { useCarritoContext } from "../../context/carrito/CarritoState";
-import useForm from "../../hooks/useForm";
-import { useDepartamentos } from "../../services/useDepartamentos";
-import { useDistritos } from "../../services/useDistritos";
-import { usePedido } from "../../services/usePedido";
-import { useProvincias } from "../../services/useProvincias";
-import InputFloat from "../inputs/InputFloat";
-import Select from "../select";
+import { SelectSearch } from '@components/SelectSearch'
+import { useRouter } from 'next/router'
+import React, { ChangeEvent, Dispatch, SetStateAction, useState } from 'react'
+import { useEffect } from 'react'
+import { useCarritoContext } from '../../context/carrito/CarritoState'
+import useForm from '../../hooks/useForm'
+import { useDepartamentos } from '../../services/useDepartamentos'
+import { useDistritos } from '../../services/useDistritos'
+import { usePedido } from '../../services/usePedido'
+import { useProvincias } from '../../services/useProvincias'
+import InputFloat from '../inputs/InputFloat'
+import Select from '../select'
 
 interface PayProps {
-  payment_method_id: string;
-  token: string;
-  installments: number;
+	payment_method_id: string
+	token: string
+	installments: number
 }
 
 interface IProps {
-  setShow: React.Dispatch<React.SetStateAction<string>>;
-  venta: string;
-  depa: string;
-  prov: string;
-  dist: string;
-  ruc: string;
-  razonSocial: string;
-  celular: string;
-  direccion: string;
-  onChange: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
-  setStateMutation: (key: string, value: string | number) => void;
+	setShow: React.Dispatch<React.SetStateAction<string>>
+	venta: string
+	depa: string
+	prov: string
+	dist: string
+	ruc: string
+	razonSocial: string
+	celular: string
+	direccion: string
+	onChange: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void
+	setStateMutation: (key: string, value: string | number) => void
 }
 
-const FormEnvio = ({
-  setShow,
-  ruc,
-  razonSocial,
-  celular,
-  direccion,
-  venta,
-  depa,
-  prov,
-  dist,
-  onChange,
-  setStateMutation,
-}: IProps) => {
-  const { db: departamentos } = useDepartamentos();
-  const { db: provincias } = useProvincias({ DepCode: depa.toString() });
-  const { db: distritos } = useDistritos({ ProCode: prov.toString() });
+const FormEnvio = ({ setShow, ruc, razonSocial, celular, direccion, venta, depa, prov, dist, onChange, setStateMutation }: IProps) => {
+	const { db: departamentos } = useDepartamentos()
+	const { db: provincias } = useProvincias({ DepCode: depa.toString() })
+	const { db: distritos } = useDistritos({ ProCode: prov.toString() })
 
-  const dataDepartamentos = departamentos.map((depa) => ({
-    value: depa?.DeparCodi!,
-    titulo: depa?.DeparNom!,
-  }));
+	const dataDepartamentos = departamentos.map((depa) => ({
+		value: depa?.DeparCodi!,
+		titulo: depa?.DeparNom!
+	}))
 
-  const dataProvincias = provincias.map((prov) => ({
-    value: prov?.ProvCodi!,
-    titulo: prov?.ProvNom!,
-  }));
-  const dataDistritos = distritos.map((prov) => ({
-    value: prov?.DistCodi!,
-    titulo: prov?.DistNom!,
-  }));
+	const dataProvincias = provincias.map((prov) => ({
+		value: prov?.ProvCodi!,
+		titulo: prov?.ProvNom!
+	}))
+	const dataDistritos = distritos.map((prov) => ({
+		value: prov?.DistCodi!,
+		titulo: prov?.DistNom!
+	}))
 
-  useEffect(() => {}, [depa, prov, dist]);
+	useEffect(() => {}, [depa, prov, dist])
 
-  return (
-    <div className="flex justify-center ">
-      <div className="w-full md:w-2/5 h-auto ">
-        <h2 className="text-primary-300 font-bold text-4xl xl:text-6xl text-center  ">
-          Completa tu información
-        </h2>
-        <div>
-          <form
-            onSubmit={() => setShow("pagar")}
-            className="flex flex-col gap-y-5 mt-10  "
-          >
-            <Select
-              required
-              label="Tipo de recibo"
-              name="venta"
-              value={venta}
-              onChange={onChange}
-              data={[
-                { value: "boleta", titulo: "Boleta" },
-                { value: "factura", titulo: "Factura" },
-              ]}
-            />
+	return (
+		<div className='flex justify-center '>
+			<div className='w-full h-auto md:w-2/5 '>
+				<h2 className='text-4xl font-bold text-center text-primary-300 xl:text-6xl '>Completa tu información</h2>
+				<div>
+					<form onSubmit={() => setShow('pagar')} className='flex flex-col mt-10 gap-y-5 '>
+						<Select
+							required
+							label='Tipo de recibo'
+							name='venta'
+							value={venta}
+							onChange={onChange}
+							data={[
+								{ value: 'boleta', titulo: 'Boleta' },
+								{ value: 'factura', titulo: 'Factura' }
+							]}
+						/>
 
-            {/* <SelectSearch
+						{/* <SelectSearch
               tittle="Tipo de recibo"
               data={[
                 { value: "boleta", titulo: "Boleta" },
@@ -98,33 +81,24 @@ const FormEnvio = ({
               setStateMutation={setStateMutation}
             /> */}
 
-            {venta === "factura" && (
-              <>
-                <InputFloat
-                  required
-                  type="text"
-                  label="Ruc"
-                  name="ruc"
-                  id="ruc"
-                  htmlFor="ruc"
-                  value={ruc}
-                  onChange={onChange}
-                />
-                <InputFloat
-                  required
-                  type="text"
-                  label="Razon Social"
-                  name="razonSocial"
-                  id="razonSocial"
-                  htmlFor="razonSocial"
-                  value={razonSocial}
-                  onChange={onChange}
-                />
-              </>
-            )}
+						{venta === 'factura' && (
+							<>
+								<InputFloat required type='text' label='Ruc' name='ruc' id='ruc' htmlFor='ruc' value={ruc} onChange={onChange} />
+								<InputFloat
+									required
+									type='text'
+									label='Razon Social'
+									name='razonSocial'
+									id='razonSocial'
+									htmlFor='razonSocial'
+									value={razonSocial}
+									onChange={onChange}
+								/>
+							</>
+						)}
 
-            <div className="flex flex-col xl:flex-row gap-5 ">
-              {/* <Select
+						<div className='flex flex-col gap-5 xl:flex-row '>
+							{/* <Select
                 required
                 label="Departamento"
                 name="depa"
@@ -154,68 +128,67 @@ const FormEnvio = ({
               />
               */}
 
-              <SelectSearch
-                tittle="Departamento"
-                data={dataDepartamentos}
-                name="depa"
-                value={depa}
-                setStateMutation={setStateMutation}
-              />
+							<SelectSearch
+								tittle='Departamento'
+								data={dataDepartamentos}
+								name='depa'
+								value={depa}
+								setStateMutation={setStateMutation}
+							/>
 
-              <SelectSearch
-                tittle="Provincia"
-                data={dataProvincias}
-                name="prov"
-                value={prov}
-                disabled={depa ? false : true}
-                setStateMutation={setStateMutation}
-              />
-              <SelectSearch
-                tittle="Distrito"
-                data={dataDistritos}
-                name="dist"
-                value={dist}
-                disabled={prov ? false : true}
-                setStateMutation={setStateMutation}
-              />
-            </div>
+							<SelectSearch
+								tittle='Provincia'
+								data={dataProvincias}
+								name='prov'
+								value={prov}
+								disabled={depa ? false : true}
+								setStateMutation={setStateMutation}
+							/>
+							<SelectSearch
+								tittle='Distrito'
+								data={dataDistritos}
+								name='dist'
+								value={dist}
+								disabled={prov ? false : true}
+								setStateMutation={setStateMutation}
+							/>
+						</div>
 
-            <InputFloat
-              required
-              type="number"
-              label="Celular"
-              name="celular"
-              id="celular"
-              htmlFor="celular"
-              value={celular}
-              maxLength={9}
-              onChange={onChange}
-            />
-            <InputFloat
-              required
-              type="text"
-              label="Dirección"
-              name="direccion"
-              id="direccion"
-              htmlFor="direccion"
-              maxLength={255}
-              value={direccion}
-              onChange={onChange}
-            />
+						<InputFloat
+							required
+							type='number'
+							label='Celular'
+							name='celular'
+							id='celular'
+							htmlFor='celular'
+							value={celular}
+							maxLength={9}
+							onChange={onChange}
+						/>
+						<InputFloat
+							required
+							type='text'
+							label='Dirección'
+							name='direccion'
+							id='direccion'
+							htmlFor='direccion'
+							maxLength={255}
+							value={direccion}
+							onChange={onChange}
+						/>
 
-            <div className="flex justify-center pt-5">
-              <button
-                type="submit"
-                className="bg-primary-300 text-white border-2 px-10 py-4 rounded-full uppercase duration-300 ease-in-out hover:border-primary-300  hover:bg-white hover:text-primary-300"
-              >
-                Continuar
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-};
+						<div className='flex justify-center pt-5'>
+							<button
+								type='submit'
+								className='px-10 py-4 text-white uppercase duration-300 ease-in-out border-2 rounded-full bg-primary-300 hover:border-primary-300 hover:bg-white hover:text-primary-300'>
+								Continuar
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default FormEnvio;
+export default FormEnvio
--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,62 +1,55 @@
-import React, {
-  ChangeEvent,
-  Dispatch,
-  InputHTMLAttributes,
-  SetStateAction,
-  useEffect
-} from 'react'
+import React, { ChangeEvent, Dispatch, InputHTMLAttributes, SetStateAction, useEffect } from 'react'
 
 interface IProps extends InputHTMLAttributes<HTMLSelectElement> {
-  value: string
-  label: string
-  onChange?: (e: ChangeEvent<HTMLSelectElement>) => void
-  data: Options[]
+	value: string
+	label: string
+	onChange?: (e: ChangeEvent<HTMLSelectElement>) => void
+	data: Options[]
 }
 
 interface Options {
-  value: string
-  titulo: string
+	value: string
+	titulo: string
 }
 
 const Select = ({
-  value,
-  label,
-  onChange,
-  data,
+	value,
+	label,
+	onChange,
+	data,
 
-  ...props
+	...props
 }: IProps) => {
-  // useEffect(() => {
-  //   if (!data[0]) {
-  //     value('')
-  //   }
-  // }, [data])
+	// useEffect(() => {
+	//   if (!data[0]) {
+	//     value('')
+	//   }
+	// }, [data])
 
-  return (
-    <div className='relative w-full '>
-      <select
-        {...props}
-        className='w-full block px-2 bg-transparent  pb-2 pt-5 text-sm text-gray-900 font-semibold border border-gray-300 focus:outline-none focus:ring-0 focus:border-primary-300 peer rounded-md'
-        value={value}
-        onChange={onChange}
-      >
-        <option value='' hidden></option>
-        {data.map((item) => (
-          <option key={item.value} value={item.value}>
-            {item.titulo}
-          </option>
-        ))}
-      </select>
+	return (
+		<div className='relative w-full '>
+			<select
+				{...props}
+				autoComplete='off'
+				className='block w-full px-2 pt-5 pb-2 text-sm font-semibold text-gray-900 bg-transparent border border-gray-300 rounded-md focus:outline-none focus:ring-0 focus:border-primary-300 peer'
+				value={value}
+				onChange={onChange}>
+				<option value='' hidden></option>
+				{data.map((item) => (
+					<option key={item.value} value={item.value}>
+						{item.titulo}
+					</option>
+				))}
+			</select>
 
-      <label
-        className={`${
-          value.length ? 'top-0 text-xs ' : 'top-4 text-base'
-        } absolute text-sm text-primary-300 bg-white -z-10 font-semibold duration-300 transform left-2.5 leading-5`}
-      >
-        {label}
-      </label>
-    </div>
-  )
+			<label
+				className={`${
+					value.length ? 'top-0 text-xs ' : 'top-4 text-base'
+				} absolute text-sm text-primary-300 bg-white -z-10 font-semibold duration-300 transform left-2.5 leading-5`}>
+				{label}
+			</label>
+		</div>
+	)
 }
 
 export default Select
