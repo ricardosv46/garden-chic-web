79ca8ed0af3448685096a5be08faf364c1ef31fc
ricardosv46 <ricardosv46@gmail.com>
2022-09-19 14:44:05 -0500

    feat: se muestra descripcion larga y corta en tienda y se muestra la cantidad

--- src/pages/tienda/[slug].tsx
+++ src/pages/tienda/[slug].tsx
@@ -44,39 +44,39 @@ const Productos = ({ producto }: Producto) => {
 
 	return (
 		<div>
-			<Container className='mt-6 py-4'>
+			<Container className='py-4 mt-6'>
 				<BreadCrumb />
 			</Container>
 			<ModalProduct isOpen={isOpen} onClose={() => setIsOpen(false)} data={Product?.galeria!} />
-			<Container className='lg:flex py-10 gap-10'>
+			<Container className='gap-10 py-10 lg:flex'>
 				<div className='w-full lg:w-9/12 '>
-					<div className='lg:flex gap-5'>
+					<div className='gap-5 lg:flex'>
 						<div className='flex-1'>
 							<Gallery onClick={() => setIsOpen(true)} data={Product?.galeria!} />
 						</div>
 						<div className='flex-1 '>
-							<div className='flex gap-3 items-center'>
-								<p className='text-gray-900 text-md font-semibold ease-in-out duration-300 hover:text-primary-300 cursor-pointer'>
+							<div className='flex items-center gap-3'>
+								<p className='font-semibold text-gray-900 duration-300 ease-in-out cursor-pointer text-md hover:text-primary-300'>
 									{Product?.CategoriaProducto?.titulo!}
 								</p>
-								<span className='w-1 h-1 bg-primary-300 rounded-full'></span>
+								<span className='w-1 h-1 rounded-full bg-primary-300'></span>
 							</div>
-							<p className='text-gray-900 text-3xl  font-bold mt-3'>{Product?.titulo!}</p>
+							<p className='mt-3 text-3xl font-bold text-gray-900'>{Product?.titulo!}</p>
 							<div className='w-5 h-0.5 bg-primary-300 my-5'></div>
-							<p className='text-gray-900 text-5xl  '>S/ {Product?.precioOferta!}</p>
+							<p className='text-5xl text-gray-900 '>S/ {Product?.precioOferta!}</p>
 
-							<p className='text-gray-900 text-lg  my-5'>{Product?.descripcionLarga!}</p>
+							<p className='my-5 text-lg text-gray-900'>{Product?.descripcionCorta!}</p>
 							<div className='flex justify-between border-2'>
-								<div className='flex-1 flex justify-center items-center'>
-									<input type='number' min='1' placeholder='1' className='w-16 p-2 focus:outline-none border-2 rounded-lg' />
+								<div className='flex items-center justify-center flex-1'>
+									<input type='number' min='1' placeholder='1' className='w-16 p-2 border-2 rounded-lg focus:outline-none' />
 								</div>
 
-								<p className='flex-1 p-5 border-l-2 text-primary-300 text-center'>{Product?.titulo}</p>
-								<p className='flex-1 p-5 border-l-2'>$20.00</p>
+								<p className='flex-1 p-5 text-center border-l-2 text-primary-300'>Cantidad</p>
+								<p className='flex-1 p-5 border-l-2'>{Product?.stockReal!}</p>
 							</div>
 
 							<button
-								className='mt-5 py-3 px-5 border-primary-300 bg-primary-300 border-2 hover:bg-white ease-in-out duration-300 text-white hover:text-primary-300 font-bold text-sm rounded-lg'
+								className='px-5 py-3 mt-5 text-sm font-bold text-white duration-300 ease-in-out border-2 rounded-lg border-primary-300 bg-primary-300 hover:bg-white hover:text-primary-300'
 								onClick={() => {
 									agregarCarrito({
 										id: Number(Product.productoId!),
@@ -95,10 +95,10 @@ const Productos = ({ producto }: Producto) => {
 						</div>
 					</div>
 					<div>
-						<p className='text-gray-900 text-3xl  font-bold mt-10'>Descripción</p>
-						<p className='text-gray-900 text-lg  my-5'>{Product?.descripcionLarga}</p>
-						<p className='text-gray-900 text-3xl  font-bold mt-10'>También te puede interesar</p>
-						<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5  justify-items-center py-10'>
+						<p className='mt-10 text-3xl font-bold text-gray-900'>Descripción</p>
+						<p className='my-5 text-lg text-gray-900'>{Product?.descripcionLarga}</p>
+						<p className='mt-10 text-3xl font-bold text-gray-900'>También te puede interesar</p>
+						<div className='grid grid-cols-1 gap-5 py-10 md:grid-cols-2 lg:grid-cols-3 justify-items-center'>
 							{productos.map((item, i) => {
 								if (i < 3) {
 									return (
@@ -120,8 +120,8 @@ const Productos = ({ producto }: Producto) => {
 								}
 							})}
 						</div>
-						<p className='text-gray-900 text-3xl  font-bold mt-10'>Productos relacionados</p>
-						<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5  justify-items-center py-10'>
+						<p className='mt-10 text-3xl font-bold text-gray-900'>Productos relacionados</p>
+						<div className='grid grid-cols-1 gap-5 py-10 md:grid-cols-2 lg:grid-cols-3 justify-items-center'>
 							{productos.map((item, i) => {
 								if (i > 2 && i < 5) {
 									return (
@@ -146,9 +146,9 @@ const Productos = ({ producto }: Producto) => {
 					</div>
 				</div>
 
-				<div className='w-full lg:w-3/12 px-3'>
-					<InputSearch placeholder='Buscar' onClick={()=>console.log('buscando publicaciones recientes')} />
-					<p className='text-gray-900 text-2xl font-bold py-10'>Productos similares</p>
+				<div className='w-full px-3 lg:w-3/12'>
+					<InputSearch placeholder='Buscar' onClick={() => console.log('buscando publicaciones recientes')} />
+					<p className='py-10 text-2xl font-bold text-gray-900'>Productos similares</p>
 					{productos.map((item, i) => {
 						if (i < 5) {
 							return (
