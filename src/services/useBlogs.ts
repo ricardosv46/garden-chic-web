bd93d91f15553b0f3c8c8ddf6722d0624bff0151
ricardosv46 <ricardosv46@gmail.com>
2022-09-22 14:01:58 -0500

    feat, se muestra solo productos activados

--- src/services/useBlogs.ts
+++ src/services/useBlogs.ts
@@ -1,22 +1,20 @@
 import { useGetAllBlogsQuery } from '../generated/graphql'
 
 // Obtenemos todas los blogs
-export const useBlogs = (
-  input = { pagina: 1, estado: '', numeroPagina: 10 }
-) => {
-  const { data, loading, refetch } = useGetAllBlogsQuery({
-    fetchPolicy: 'network-only',
-    variables: {
-      ...input
-    }
-  })
+export const useBlogs = (input = { pagina: 1, estado: 'Activado', numeroPagina: 10 }) => {
+	const { data, loading, refetch } = useGetAllBlogsQuery({
+		fetchPolicy: 'network-only',
+		variables: {
+			...input
+		}
+	})
 
-  const db = data?.GetAllBlogs?.data ?? []
-  const nTotal = data?.GetAllBlogs?.numeroTotal ?? 0
+	const db = data?.GetAllBlogs?.data ?? []
+	const nTotal = data?.GetAllBlogs?.numeroTotal ?? 0
 
-  return {
-    loading,
-    db,
-    nTotal
-  }
+	return {
+		loading,
+		db,
+		nTotal
+	}
 }
--- src/services/useProducto.ts
+++ src/services/useProducto.ts
@@ -1,22 +1,20 @@
 import { useGetAllProductosQuery } from '../generated/graphql'
 
 // Obtenemos todas los blogs
-export const useProductos = (
-  input = { pagina: 1, estado: '', numeroPagina: 10 }
-) => {
-  const { data, loading, refetch } = useGetAllProductosQuery({
-    fetchPolicy: 'network-only',
-    variables: {
-      ...input
-    }
-  })
+export const useProductos = (input = { pagina: 1, estado: 'Activado', numeroPagina: 10 }) => {
+	const { data, loading, refetch } = useGetAllProductosQuery({
+		fetchPolicy: 'network-only',
+		variables: {
+			...input
+		}
+	})
 
-  const db = data?.GetAllProductos?.data ?? []
-  const nTotal = data?.GetAllProductos?.numeroTotal ?? 0
+	const db = data?.GetAllProductos?.data ?? []
+	const nTotal = data?.GetAllProductos?.numeroTotal ?? 0
 
-  return {
-    loading,
-    db,
-    nTotal
-  }
+	return {
+		loading,
+		db,
+		nTotal
+	}
 }
